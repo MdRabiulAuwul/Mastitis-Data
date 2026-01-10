77570958cb9353914fdea11f134f891b6f759e55
Md. Rabiul Auwul, PhD <62581890+MdRabiulAuwul@users.noreply.github.com>
2026-01-10 11:52:37 +0600

    Integrate GSE15025 analysis and ML models
    Added differential expression analysis for GSE15025 using limma and DESeq2. Included machine learning models for classification of DE genes.

--- R code (mastitis DE gene identification and validation).R
+++ R code (mastitis DE gene identification and validation).R
@@ -1,13 +1,51 @@
-library(limma)
+
 library(DESeq2)
-library(edgeR)
 
-Data1 <- read.csv("GSE64801.csv")
-Data<-Data1[,-1]
-rownames(Data)<-Data1[,1]
+#   Differential expression analysis with limma for GSE15025
+library(GEOquery)
+library(limma)
 
-group<-factor(rep(c("1","0"),c(9,5)))
-design<-model.matrix(~ group)
+# load series and platform data from GEO
+
+gset <- getGEO("GSE15025", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL2112", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# make proper column names to match toptable 
+fvarLabels(gset) <- make.names(fvarLabels(gset))
+
+# group membership for all samples
+gsms <- "000001111100000111110000XXXXXX"
+sml <- strsplit(gsms, split="")[[1]]
+
+# filter out excluded samples (marked as "X")
+sel <- which(sml != "X")
+sml <- sml[sel]
+gset <- gset[ ,sel]
+
+# log2 transformation
+ex <- exprs(gset)
+qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+LogC <- (qx[5] > 100) ||
+  (qx[6]-qx[1] > 50 && qx[2] > 0)
+if (LogC) { ex[which(ex <= 0)] <- NaN
+exprs(gset) <- log2(ex) }
+
+# assign samples to groups and set up design matrix
+gs <- factor(sml)
+groups <- make.names(c("T","C"))
+batch <- pData(GSE15025)$time.point  # or $characteristics_ch1 (check actual metadata)
+design <- model.matrix(~ batch + group1 + 0)  
+colnames(design) <- gsub("^batch", "B", colnames(design))
+colnames(design) <- gsub("^group", "G", colnames(design))
+Data_corrected <- removeBatchEffect(scale(Data), batch=batch, design=model.matrix(~ group))
+fit <- lmFit(scale(Data), design)
+cont.matrix <- makeContrasts(G1-G0, levels=design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2 <- eBayes(fit2)
+tT <- topTable(fit2, adjust="BH", n = nrow(Data), sort.by = "none")
+pval <- tT$P.Value
+TopDEGT <- which(tT$adj.P.Val < 0.05 & abs(tT$logFC) >= 0.5)
 
 ######---------S-S (DESeq2)------##########
 
@@ -18,7 +56,7 @@ ssR <- DESeq2::estimateDispersions(ssR, fitType = "parametric")
 ssR <- DESeq2::nbinomLRT(ssR, full = ~ condition, reduced = ~ 1)
 res <- results(ssR)
 
-#############ML##########
+#############Machine Learning##########
 DataX<-Data[,res] ##Extract only DE gene
 library(class)
 library(caret)
